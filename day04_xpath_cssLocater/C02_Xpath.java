cb3a570cb20ffad3250134c3e7c67a30e0f087aa
samet ozcelik <smtab142790@gmail.com>
2023-04-23 15:02:51 +0300

    23/04/2023

---
+++ day04_xpath_cssLocater/C02_Xpath.java
@@ -0,0 +1,24 @@
+package day04_xpath_cssLocater;
+
+public class C02_Xpath {
+    public static void main(String[] args) {
+        /*
+        <img class="s-image" src="https://m.media-amazon.com/images/I/71kA0co7xwS._AC_UY218_.jpg"
+        srcset="https://m.media-amazon.com/images/I/71kA0co7xwS._AC_UY218_.jpg 1x,
+        https://m.media-amazon.com/images/I/71kA0co7xwS._AC_UY327_FMwebp_QL65_.jpg 1.5x,
+        https://m.media-amazon.com/images/I/71kA0co7xwS._AC_UY436_FMwebp_QL65_.jpg 2x,
+        https://m.media-amazon.com/images/I/71kA0co7xwS._AC_UY545_FMwebp_QL65_.jpg 2.5x,
+        https://m.media-amazon.com/images/I/71kA0co7xwS._AC_UY654_FMwebp_QL65_.jpg 3x"
+         alt="Sponsored Ad - Retrospec Lennon Bike Helmet with LED Safety Light Adjustable Dial &amp; Removable Visor -
+        Adjustable Bicycle H..." data-image-index="1" data-image-load=""
+        data-image-latency="s-product-image" data-image-source-density="1">
+         */
+        /*
+        yukaridaki webElement de unique olmasi icin
+                  //tag name[@attribute ismi='attributeValue'] şeklinde yazılır. hala tek bulamaz isek index in al
+                  //tag name[@attribute ismi='attributeValue'][1]       şeklinde yazılır
+                  //img[@class='s-image'] dir
+         */
+
+    }
+}
