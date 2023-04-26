ffc144e56c0d3737a2def265a926eab79a90f13d
samet ozcelik <smtab142790@gmail.com>
2023-04-26 23:36:28 +0300

    26/04/2023

---
+++ day11_faker_file/C03_FileExist.java
@@ -0,0 +1,18 @@
+package day11_faker_file;
+
+import org.junit.Test;
+
+public class C03_FileExist {
+
+    @Test
+    public void test01() {
+        System.out.println(System.getProperty("user.dir"));
+        ///Users/fadimeozcelik/IdeaProjects/com.BatchJUnit
+        //bana icinde oldugum projenin dosya yolunu(path) verir
+
+        System.out.println(System.getProperty("user.home"));
+        // /Users/fadimeozcelik yani benim bilgisayarimin bana ozel kismini verdi
+
+
+    }
+}
